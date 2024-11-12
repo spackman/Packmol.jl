ae03dc75a3763508630dd8383fbc375ec54681ef
Isaac Spackman <87101526+spackman@users.noreply.github.com>
2024-11-12 04:30:51 +0000

    remove NativeFileDialog from packmol_runner.jl

--- src/packmol_runner.jl
+++ src/packmol_runner.jl
@@ -1,21 +1,16 @@
 import Packmol_jll
-import NativeFileDialog
 
 packmol_runner = Packmol_jll.packmol()
 
 export run_packmol
 
 """
-    run_packmol()
     run_packmol(input_file::String)
 
 Runs the packmol executable with the input file `input_file`. This will run
 the classical `http://m3g.iqm.unicamp.br/packmol` program, which is a
 pre-compiled binary. The input file is a text file with the same syntax as
 the packmol input files.
-
-If no input file is provided, a file explorer will be opened to choose the 
-input file.
 
 """
 function run_packmol end
@@ -51,11 +46,6 @@ function run_packmol(input_file::String)
     return nothing
 end
 
-@doc (@doc run_packmol)
-function run_packmol()
-    input_file = NativeFileDialog.pick_file() 
-    run_packmol(input_file)
-end
 
 @testitem "run_packmol" begin
     using Packmol
